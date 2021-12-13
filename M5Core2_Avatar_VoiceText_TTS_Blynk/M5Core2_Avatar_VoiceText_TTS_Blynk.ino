c0fd82d18645d6adfcd7055a5f3546b2f48c1dad
robo8080 <motm_rbt@yahoo.co.jp>
2021-12-13 16:02:28 +0900

    initial commit

---
+++ M5Core2_Avatar_VoiceText_TTS_Blynk/M5Core2_Avatar_VoiceText_TTS_Blynk.ino
@@ -0,0 +1,218 @@
+#include <M5Core2.h>
+#include <Avatar.h>
+
+#include "AudioFileSourceBuffer.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2SLipSync.h"
+#include "AudioFileSourceVoiceTextStream.h"
+
+#include <BlynkSimpleEsp32.h> // Blynk for esp32
+
+using namespace m5avatar;
+
+Avatar avatar;
+
+const char *SSID = "YOUR_WIFI_SSID";
+const char *PASSWORD = "YOUR_WIFI_PASSWORD";
+
+// #Blynk
+// You should get Auth Token.
+// visit https://docs.blynk.cc/
+const char* auth = "YOUR_BLYNK_AUTH_TOKEN";
+
+AudioGeneratorMP3 *mp3;
+AudioFileSourceVoiceTextStream *file;
+AudioFileSourceBuffer *buff;
+AudioOutputI2SLipSync *out;
+const int preallocateBufferSize = 40*1024;
+uint8_t *preallocateBuffer;
+
+// Called when a metadata event occurs (i.e. an ID3 tag, an ICY block, etc.
+void MDCallback(void *cbData, const char *type, bool isUnicode, const char *string)
+{
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  (void) isUnicode; // Punt this ball for now
+  // Note that the type and string may be in PROGMEM, so copy them to RAM for printf
+  char s1[32], s2[64];
+  strncpy_P(s1, type, sizeof(s1));
+  s1[sizeof(s1)-1]=0;
+  strncpy_P(s2, string, sizeof(s2));
+  s2[sizeof(s2)-1]=0;
+  Serial.printf("METADATA(%s) '%s' = '%s'\n", ptr, s1, s2);
+  Serial.flush();
+}
+
+// Called when there's a warning or error (like a buffer underflow or decode hiccup)
+void StatusCallback(void *cbData, int code, const char *string)
+{
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  // Note that the string may be in PROGMEM, so copy it to RAM for printf
+  char s1[64];
+  strncpy_P(s1, string, sizeof(s1));
+  s1[sizeof(s1)-1]=0;
+  Serial.printf("STATUS(%s) '%d' = '%s'\n", ptr, code, s1);
+  Serial.flush();
+}
+
+void lipSync(void *args)
+{
+  DriveContext *ctx = (DriveContext *)args;
+  Avatar *avatar = ctx->getAvatar();
+   for (;;)
+  {
+    int level = out->getLevel();
+    level = abs(level);
+    if(level > 10000)
+    {
+      level = 10000;
+    }
+    float open = (float)level/10000.0;
+    avatar->setMouthOpenRatio(open);
+
+    delay(33);
+//    delay(50);
+  }
+}
+
+void setup()
+{
+  preallocateBuffer = (uint8_t*)ps_malloc(preallocateBufferSize);
+  M5.begin(true, false, true);
+  M5.Axp.SetSpkEnable(true);
+  M5.Lcd.setBrightness(30);
+  M5.Lcd.clear();
+  M5.Lcd.setTextSize(2);
+  delay(1000);
+
+  Serial.println("Connecting to WiFi");
+  M5.Lcd.print("Connecting to WiFi");
+  WiFi.disconnect();
+  WiFi.softAPdisconnect(true);
+  WiFi.mode(WIFI_STA);
+//  WiFi.begin(SSID, PASSWORD);
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(250);
+//    Serial.print(".");
+//    M5.Lcd.print(".");
+//  }
+  Blynk.begin(auth, SSID, PASSWORD);
+  Serial.println("\nConnected");
+  M5.Lcd.println("\nConnected");
+  
+  audioLogger = &Serial;
+  out = new AudioOutputI2SLipSync();
+  out->SetPinout(12, 0, 2);           // ピン配列を指定（BCK, LRCK, DATA)BashCopy
+  mp3 = new AudioGeneratorMP3();
+  mp3->RegisterStatusCB(StatusCallback, (void*)"mp3");
+
+  avatar.init();
+  avatar.addTask(lipSync, "lipSync");
+}
+
+char *text1 = "こんにちは、世界！";
+char *tts_parms1 ="&emotion_level=2&emotion=happiness&format=mp3&speaker=hikari&volume=200&speed=120&pitch=130";
+char *tts_parms2 ="&emotion_level=2&emotion=happiness&format=mp3&speaker=takeru&volume=200&speed=100&pitch=130";
+char *tts_parms3 ="&emotion_level=4&emotion=anger&format=mp3&speaker=bear&volume=200&speed=120&pitch=100";
+
+void VoiceText_tts(char *text,char *tts_parms) {
+    file = new AudioFileSourceVoiceTextStream( text, tts_parms);
+    buff = new AudioFileSourceBuffer(file, preallocateBuffer, preallocateBufferSize);
+    delay(100);
+    mp3->begin(buff, out);
+}
+
+// Blynk 
+// use 'ping blynk-cloud.com' to find ip address
+// http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v1?value=hello%20world
+BLYNK_WRITE(V1)
+{
+  String pinValue = param.asStr();
+  char text[pinValue.length() + 1];
+  pinValue.toCharArray(text, pinValue.length() + 1);
+  
+  Serial.print("BLYNK_WRITE(V1): ");
+  Serial.println(pinValue);
+  
+  avatar.setExpression(Expression::Happy);
+  VoiceText_tts(text, tts_parms1);
+  avatar.setExpression(Expression::Neutral);
+
+}
+
+// Blynk 
+// use 'ping blynk-cloud.com' to find ip address
+// http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v2?value=hello%20world
+BLYNK_WRITE(V2)
+{
+  String pinValue = param.asStr();
+  char text[pinValue.length() + 1];
+  pinValue.toCharArray(text, pinValue.length() + 1);
+  
+  Serial.print("BLYNK_WRITE(V2): ");
+  Serial.println(pinValue);
+  
+  avatar.setExpression(Expression::Happy);
+  VoiceText_tts(text, tts_parms2);
+  avatar.setExpression(Expression::Neutral);
+  
+}
+
+// Blynk 
+// use 'ping blynk-cloud.com' to find ip address
+// http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v3?value=hello%20world
+BLYNK_WRITE(V3)
+{
+  String pinValue = param.asStr();
+  char text[pinValue.length() + 1];
+  pinValue.toCharArray(text, pinValue.length() + 1);
+  
+  Serial.print("BLYNK_WRITE(V3): ");
+  Serial.println(pinValue);
+  
+  avatar.setExpression(Expression::Happy);
+  VoiceText_tts(text, tts_parms3);
+  avatar.setExpression(Expression::Neutral);
+  
+}
+
+void loop()
+{
+  static int lastms = 0;
+  Blynk.run();
+  M5.update();
+  if (M5.BtnA.wasPressed())
+  {
+    avatar.setExpression(Expression::Happy);
+    VoiceText_tts(text1, tts_parms1);
+    avatar.setExpression(Expression::Neutral);
+    Serial.println("mp3 begin");
+  }
+  if (M5.BtnB.wasPressed())
+  {
+    avatar.setExpression(Expression::Happy);
+    VoiceText_tts(text1, tts_parms2);
+    avatar.setExpression(Expression::Neutral);
+    Serial.println("mp3 begin");
+  }
+  if (M5.BtnC.wasPressed())
+  {
+    avatar.setExpression(Expression::Happy);
+    VoiceText_tts(text1, tts_parms3);
+    avatar.setExpression(Expression::Neutral);
+    Serial.println("mp3 begin");
+  }
+  if (mp3->isRunning()) {
+    if (millis()-lastms > 1000) {
+      lastms = millis();
+      Serial.printf("Running for %d ms...\n", lastms);
+      Serial.flush();
+     }
+    if (!mp3->loop()) {
+      mp3->stop();
+      out->setLevel(0);
+      delete file;
+      delete buff;
+      Serial.println("mp3 stop");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # M5Core2_Avatar_VoiceText_TTS_Blynk
 M5Core2_Avatar_VoiceText_TTS_Blynk
+M5Stack Core2で、M5Stack-AvatarとHOYA社が提供する[VoiceText Web APIサービス](https://cloud.voicetext.jp/webapi "Title")を使った音声合成(TTS)を動かすテストプログラムです。
+
+
+VoiceText TTSは、kghrlaboさんのesp32_text_to_speechを参考にさせていただきました。<br>
+オリジナルはこちら。<br>
+esp32_text_to_speech <https://github.com/kghrlabo/esp32_text_to_speech><br>
+
+---
+
+### 必要な物 ###
+* [M5Stack Core2](http://www.m5stack.com/ "Title") (M5Stack Core2、M5Stack Core2 for AWSで動作確認をしました。)<br>
+* Arduino IDE (バージョン 1.8.15で動作確認をしました。)<br>
+* [M5Stack-Avatar](https://github.com/meganetaaan/m5stack-avatar/ "Title")ライブラリ(バージョン 0.7.3で動作確認をしました。)<br>
+* [ESP8266Audio](https://github.com/earlephilhower/ESP8266Audio/ "Title")ライブラリ(バージョン 1.9.3で動作確認をしました。)<br>
+* [Blynk](https://blynk.io/ "Title")ライブラリ(バージョン 1.0.1で動作確認をしました。)<br><br>
+
+### M5Stack-Avatarライブラリの変更 ###
+* "Avatar.cpp"の下図赤枠部分を変更します。
+![画像1](images/image1.png)<br><br>
+
+### WiFiの設定 ###
+* M5Core2_Avatar_VoiceText_TTS_Blynk.inoの15行目付近、SSIDとPASSWORDを設定してください。
+
+### Blynk Auth Tokenの設定 ###
+* M5Core2_Avatar_VoiceText_TTS_Blynk.inoの21行目付近、YOUR_BLYNK_AUTH_TOKENを設定してください。<br>
+Blynk Auth Tokenは、[ここ](https://docs.blynk.cc/ "Title")から取得してください。。<br>
+
+
+### VoiceText Wev API api キーの設定 ###
+* AudioFileSourceVoiceTextStream.cppの30行目付近、YOUR_TSS_API_KEYを設定してください。<br>
+APIキーは、[ここ](https://cloud.voicetext.jp/webapi/ "Title")の「無料利用登録」から申請すれば、メールで送られて来ます。<br>
+
+### 使い方１ ###
+1. 'ping blynk-cloud.com'で、BlynkのサーバーのIPアドレスを調べてください。 <br>
+2. ブラウザから下記のようにアクセスしてください。 V1、V2、V3、それぞれ異なった声でしゃべります。<br>
+http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v1?value=こんにちは世界！<br>
+http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v2?value=こんにちは世界！<br>
+http://188.166.206.43/YOUR_BLYNK_AUTH_TOKEN/update/v3?value=こんにちは世界！<br><br>
+
+### 使い方２ ###
+* M5Stack Core2のボタンA,B,Cを押すと、それぞれ異なった声でしゃべります。　<br><br>
+
+TTSのパラメータの詳細はこちらを参照してください。<br>
+[VoiceText Web API [API マニュアル](https://cloud.voicetext.jp/webapi/docs/api/ "Title")]
+<br><br><br>
+
